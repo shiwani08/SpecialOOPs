ef177e826c1523f2ff07af601602552f9a549357
geekordian <96004052+shiwani08@users.noreply.github.com>
2024-04-06 00:47:03 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -11,4 +11,21 @@ public class Student {
         return "Student {" +
         "name:  " + name + " roll no: "+ roll + "}\n";
     }
+
+    @Override
+    public boolean equals (Object obj) {
+        if (this == obj) 
+            return true;
+        
+        if(obj == null || getClass() != obj.getClass())
+            return false;
+            
+        Student stu = (Student) obj;
+            return roll == stu.roll;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roll);
+    }
 }
